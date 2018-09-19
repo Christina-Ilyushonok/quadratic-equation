ec2a3d4bf1505c7e1952a8545efcd1fe686daf96
Christina-Ilyushonok <iluyshonok.kristina@gmail.com>
2018-09-19 18:48:58 +0300

    task 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let arrValues = equation.replace(/\s/g, '').match(/[\d+-]\d+/g);
+    const a = +(arrValues[0]);
+    const b = +(arrValues[1]);
+    const c = +(arrValues[2]);
+    const D = Math.pow(b,2) - 4*a*c;
+    const x1 = Math.round((-1)*b + Math.sqrt(D)) / (2*a);
+    const x2 = Math.round((-1)*b - Math.sqrt(D)) / (2*a);
+    let res = [];
+    return res.concat(x1, x2).sort((a, b) => a - b);
 }
